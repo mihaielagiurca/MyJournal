9f05517a085f9013d9ee95e5cf2e07e08f895f86
mihaielagiurca <33603566+mihaielagiurca@users.noreply.github.com>
2023-05-19 17:07:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,22 @@
-const post = document.getElementById('btn');
+const showAdditionalMedia = document.getElementsByClassName("showAdditionalMedia");
+const morePosts = document.getElementById('btn-view-more');
+const lessPosts = document.getElementById('btn-view-less');
 
-post.addEventListener('click', function(){
-    document.getElementById('morePosts').innerHTML = renderPosts();
+
+morePosts.addEventListener('click', function(){
+    const elementsToShow = document.querySelectorAll(".showAdditionalMedia");
+    elementsToShow.forEach(function(item){
+        item.style.display = "block";
+    })
+    morePosts.style.display = "none";   
+    lessPosts.style.display = "block";
 })
 
-function renderPosts(){
-    let posts = '';
-    posts = `   <div class="blog">
-                            <img src="images/coding1.avif" alt="Image with computer on a desk">
-                            <p>MAY 16, 2023</p>
-                            <h2>Blog Four</h2>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur sunt cum placeat molestias suscipit. 
-                                At quaerat reiciendis provident veritatis nihil aliquam ullam fugit ipsam est consequatur earum laborum, animi ab.</p>
-                        </div>
-                        <div class="blog">
-                            <img src="images/computer.avif" alt="Image with computer on a desk">
-                            <p>MAY 16, 2023</p>
-                            <h2>Blog Five</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione neque incidunt vitae quas odio consectetur repellat adipisci ducimus optio, 
-                                error, fugit quisquam, ab ipsum iure ex asperiores impedit corporis provident.</p>
-                        </div>
-                        <div class="blog">
-                            <img src="images/article-image-03.png" alt="Image with computer on a desk">
-                            <p>MAY 16, 2023</p>
-                            <h2>Blog Six</h2>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem repellendus quam temporibus ut, perferendis molestias velit, 
-                                perspiciatis similique adipisci a officia qui nisi aliquid aut quod omnis sint, sed tempore!!</p>
-                        </div>
-                        `
-    return posts;
-}
+lessPosts.addEventListener('click', function(){
+    const elementsToHide = document.querySelectorAll(".showAdditionalMedia");
+    elementsToHide.forEach(function(item){
+        item.style.display = "none";
+        lessPosts.style.display = "none";
+        morePosts.style.display = "inline-block";  
+    })
+    })
